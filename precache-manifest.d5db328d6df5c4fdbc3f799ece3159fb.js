f804a15e133e3a03f07074aedecc3001564d03d9
hyosunko <hyosunko@gmail.com>
2019-01-01 20:29:47 -0800

    Updates

---
+++ precache-manifest.d5db328d6df5c4fdbc3f799ece3159fb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6ea9af9a7e82766d5d64",
+    "url": "/battleships/static/js/runtime~main.6ea9af9a.js"
+  },
+  {
+    "revision": "871229117c7a712d791d",
+    "url": "/battleships/static/js/main.87122911.chunk.js"
+  },
+  {
+    "revision": "f739feb7d61c48a46bed",
+    "url": "/battleships/static/js/1.f739feb7.chunk.js"
+  },
+  {
+    "revision": "871229117c7a712d791d",
+    "url": "/battleships/static/css/main.dac5c154.chunk.css"
+  },
+  {
+    "revision": "456d31b04ce5f35c3be44c7112dabd33",
+    "url": "/battleships/index.html"
+  }
+];
